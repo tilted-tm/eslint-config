63f8396211546b66de6f6574473be16b8ef5867e
Axel Vencatareddy <axel@tilted.fr>
2021-11-19 12:34:06 +0100

    Add jsx-a11y devDependencies and remove standard-react-native config

--- react-native.js
+++ react-native.js
@@ -1,10 +1,15 @@
 module.exports = {
   extends: [
-    './react.js',
-    'standard-react-native'
+    './react.js'
   ],
   plugins: [
     'react-native',
     '@react-native-community'
-  ]
+  ],
+  rules: {
+    'react-native/no-unused-styles': 'error',
+    'react-native/split-platform-components': 'error',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-color-literals': 'error'
+  }
 }
