717720bfc9bf2898075e83db207bc171ef7458d3
Axel Vencatareddy <axel@tilted.fr>
2022-09-07 12:08:05 +0200

    [STYLES] Force alphabetical order on React native's Stylesheet

--- react-native.js
+++ react-native.js
@@ -7,9 +7,10 @@ module.exports = {
     '@react-native-community'
   ],
   rules: {
-    'react-native/no-unused-styles': 'error',
-    'react-native/split-platform-components': 'error',
+    'react-native/no-color-literals': 'error',
     'react-native/no-inline-styles': 'error',
-    'react-native/no-color-literals': 'error'
+    'react-native/no-unused-styles': 'error',
+    'react-native/sort-styles': 'error',
+    'react-native/split-platform-components': 'error'
   }
 }
