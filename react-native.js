73ed43e206336a0a2afbb446e2b0b118aa504a82
Axel Vencatareddy <axel@tilted.fr>
2022-10-27 15:28:10 +0200

    [STYLES] Disable react-native/sort-styles as sort-keys-fix is better

--- react-native.js
+++ react-native.js
@@ -10,7 +10,6 @@ module.exports = {
     'react-native/no-color-literals': 'error',
     'react-native/no-inline-styles': 'error',
     'react-native/no-unused-styles': 'error',
-    'react-native/sort-styles': 'error',
     'react-native/split-platform-components': 'error'
   }
 }
